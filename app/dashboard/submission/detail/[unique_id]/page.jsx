f9687aadc943071db45fd1148c3c00393f2e540d
Dev-FE-Superfk <dev-fe@superfk.co>
2024-06-25 16:49:56 +0700

    submission detail

--- app/dashboard/submission/detail/[unique_id]/page.jsx
+++ app/dashboard/submission/detail/[unique_id]/page.jsx
@@ -2,10 +2,56 @@
 import Image from 'next/image';
 import {ContactIcon, MailIcon, UserIcon} from '@/public';
 import {DefaultContainer, TableHome, Card} from '@/components';
+import {useParams} from 'next/navigation';
+import {useEffect, useState} from 'react';
+import Cookies from 'js-cookie';
 import Link from 'next/link';
 import '@/styles/submissionDetail.scss';
 
 export default function page() {
+  const {unique_id} = useParams();
+  const [submissionDetail, setSubmissionDetail] = useState(null);
+
+  const token = Cookies.get('token');
+  useEffect(() => {
+    if (unique_id) {
+      const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/submission/${unique_id}`;
+
+      fetch(apiUrl, {
+        method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const submissionDetail = data.data;
+
+          setSubmissionDetail(submissionDetail);
+        })
+        .catch((error) => {
+          console.error('Error fetching data: ', error);
+        });
+    }
+  }, [unique_id]);
+
+  if (!submissionDetail) {
+    return <div>Loading...</div>;
+  }
+
+  const questionDetails = submissionDetail.question_details.map((detail, index) => ({
+    number: index + 1,
+    question: detail.question,
+    answer: detail.answer,
+    tag: detail.tag,
+    duration: detail.duration,
+  }));
   return (
     <div className='flex flex-col gap-[15px]'>
       <DefaultContainer title={'Submission Detail'} />
@@ -15,31 +61,32 @@ export default function page() {
           <Image src={UserIcon} width={20} height={20} alt='profile_icon' />
           <p>PIC</p>
           <p>
-            Mr. <span className='highlight'>John Doe</span>
+            Mr. <span className='highlight'>{submissionDetail.name}</span>
           </p>
         </div>
         <div className='sd_about_content'>
           <Image src={ContactIcon} width={20} height={20} alt='profile_icon' />
           <p>Contact</p>
           <p>
-            +62 <span className='highlight'>811234124</span>
+            <span className='highlight'>{submissionDetail.phone}</span>
           </p>
         </div>
         <div className='sd_about_content'>
           <Image src={MailIcon} width={20} height={20} alt='profile_icon' />
           <p>Email</p>
           <p>
-            <span className='highlight'>johndoe@pertamina.com</span>
+            <span className='highlight'>{submissionDetail.email}</span>
           </p>
         </div>
       </div>
       <div className='sd_content_container'>
         <div className='w-full flex justify-between'>
           <p className='content_title'>Questions Details</p>
-          <Link href={`/dashboard/submission/`}>
           <button className='back_button'>
+            <Link href="/dashboard/submission/">
             <p>Back</p>
-          </button></Link>
+            </Link>
+          </button>
         </div>
         <TableHome
           tableName={[
@@ -64,47 +111,12 @@ export default function page() {
               colSpan: 1,
             },
           ]}
-          tableValue={[
-            {
-              number: 'Question 1',
-              question: '00:02',
-              answer: '00:43',
-              tag: '02:43',
-              duration: '00:02',
-            },
-            {
-              number: 'Question 2',
-              question: '00:02',
-              answer: '00:43',
-              tag: '02:43',
-              duration: '00:02',
-            },
-            {
-              number: 'Question 3',
-              question: '00:02',
-              answer: '00:43',
-              tag: '02:43',
-              duration: '00:02',
-            },
-            {
-              number: 'Question 4',
-              question: '00:02',
-              answer: '00:43',
-              tag: '02:43',
-              duration: '00:02',
-            },
-            {
-              number: 'Question 5',
-              question: '00:02',
-              answer: '00:43',
-              tag: '02:43',
-              duration: '00:02',
-            },
-          ]}
+          tableValue={questionDetails}
           tableKey={['number', 'question', 'answer', 'tag', 'duration']}
           tHeadBg={'#474747'}
         />
       </div>
+      {submissionDetail.choosen_product && (
       <div className='sd_content_container_row'>
         <div>
           <p className='choosen_text'>Choosen Product</p>
@@ -112,12 +124,13 @@ export default function page() {
         <div>
           <Card
             brand_name='Mitsubishi'
-            product_level_1_name='Pajero Dakkar'
-            product_level_2_name='Ultimate 4x4 AT'
-            price={'Rp 200.000.000'}
+            product_level_1_name={submissionDetail.choosen_product.product}
+            product_level_2_name={submissionDetail.choosen_product.variant}
+            price={`Rp ${submissionDetail.choosen_product.price.toLocaleString()}`}
           />
         </div>
       </div>
+        )}
     </div>
   );
 }
