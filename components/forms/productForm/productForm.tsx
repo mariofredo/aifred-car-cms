0b050254de40399605092f74dc1a2de991f4ac9b
mariofredo <54829954+mariofredo@users.noreply.github.com>
2024-06-26 18:04:00 +0700

    modify product form table props

--- components/forms/productForm/productForm.tsx
+++ components/forms/productForm/productForm.tsx
@@ -1,12 +1,19 @@
 'use client';
-import {CirclePlus, NoImage, PencilIcon, TrashIcon} from '@/public';
+import {
+  CirclePlus,
+  NoImage,
+  PencilIcon,
+  ReturnIcon,
+  SliderIcon,
+  TrashIcon,
+} from '@/public';
 import Image from 'next/image';
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback, useEffect, useMemo, useState} from 'react';
 import {IoSearch} from 'react-icons/io5';
-import {Table, ModalForm, Button} from '@/components';
+import {Table, ModalForm, Button, FilterModal} from '@/components';
 import {useModal, useBrand, useProduct, useSpec} from '@/context';
 import {useParams, useRouter} from 'next/navigation';
-import {Tag, Brand, Variant} from '@/types';
+import {Tag, Brand, Variant, Comparison} from '@/types';
 import Cookie from 'js-cookie';
 import '@/styles/productForm.scss';
 import Link from 'next/link';
@@ -27,7 +34,7 @@ export default function ProductForm(
   const router = useRouter();
   const {id}: {id: string} = useParams();
   const token = Cookie.get('token_aifred_neo_cms')?.toString();
-  const {showModal, setShowModal} = useModal();
+  const {showModal, setShowModal, filterModal, setFilterModal} = useModal();
   const {productId} = params;
   const {createProduct, getDetailProduct, updateProduct} = useProduct();
   const {getListBrand} = useBrand();
@@ -54,7 +61,7 @@ export default function ProductForm(
   const [tags, setTags] = useState<Tag[]>([]);
   const [suggestions, setSuggestions] = useState<Tag[]>([]);
   const [variant, setVariant] = useState<Variant[]>([]);
-  const [comparison, setComparison] = useState([]);
+  const [comparison, setComparison] = useState<Comparison[]>([]);
 
   const callListBrand = useCallback(async () => {
     const {data} = await getListBrand();
@@ -96,26 +103,6 @@ export default function ProductForm(
   //   setPayload({...payload, price: value});
   // };
 
-  const getListTag = async () => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/answer-tag`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    if (response.ok) {
-      const data = await response.json();
-      const result = await data.data.map((el: {tag: string}) => ({
-        id: el.tag,
-        text: el.tag,
-      }));
-      setSuggestions(result);
-    }
-  };
-
   const handleSelectedSpecChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
       setSelectedSpecs((prev) => {
@@ -129,7 +116,99 @@ export default function ProductForm(
     },
     [selectedSpecs, setSelectedSpecs]
   );
-
+  const handleRenderFilter = useMemo(
+    () =>
+      filterModal && (
+        <FilterModal
+          list={[
+            {
+              title: 'Brand',
+              type: 'button',
+              data: [
+                {
+                  label: 'A to Z',
+                  value: 'asc',
+                  name: 'order_by_brand',
+                },
+                {
+                  label: 'Z to A',
+                  value: 'desc',
+                  name: 'order_by_brand',
+                },
+              ],
+            },
+            {
+              title: 'Series Name',
+              type: 'button',
+              data: [
+                {
+                  label: 'A to Z',
+                  value: 'asc',
+                  name: 'order_by_series',
+                },
+                {
+                  label: 'Z to A',
+                  value: 'desc',
+                  name: 'order_by_series',
+                },
+              ],
+            },
+            {
+              title: 'Activity By Date',
+              type: 'date',
+              data: [
+                {
+                  label: 'from',
+                  name: 'date_created_start',
+                },
+                {
+                  label: 'until',
+                  name: 'date_created_end',
+                },
+              ],
+            },
+            {
+              title: 'Status',
+              type: 'status_single',
+              data: [
+                {
+                  label: 'Publish',
+                  value: 1,
+                  name: 'is_active',
+                },
+                {
+                  label: 'Unpublish',
+                  value: 0,
+                  name: 'is_active',
+                },
+              ],
+            },
+          ]}
+          setFilterModal={setFilterModal}
+          payload={payload}
+          setPayload={setPayload}
+          action={() => {
+            setFilterModal(false);
+            // callListProduct({
+            //   page: pagination.currentPage,
+            //   limit: pagination.limit,
+            //   brand_unique_id: payload.brand_unique_id?.value,
+            //   keyword: payload.keyword,
+            //   order_by_brand: payload.order_by_brand,
+            //   order_by_series: payload.order_by_series,
+            //   date_created_start: payload.date_created_start,
+            //   date_created_end: payload.date_created_end,
+            //   is_active: payload.is_active,
+            // });
+          }}
+        />
+      ),
+    [
+      payload,
+      filterModal,
+      // pagination
+    ]
+  );
   const callDetailProduct = async (id: string) => {
     const {product, competitors, variants} = await getDetailProduct(id);
     setPayload((prev) => ({
@@ -153,6 +232,7 @@ export default function ProductForm(
   return (
     <>
       <div className='grid grid-cols-3 gap-[30px]'>
+        {handleRenderFilter}
         <div className='col-span-3'>
           <div className='text-[24px] text-[#3e3e3e]'>Product Essentials</div>
           <div className='text-[12px] text-[#b5b5b5]'>
@@ -237,11 +317,7 @@ export default function ProductForm(
             <div className='col-span-3 flex gap-[20px] '>
               <button
                 className='flex justify-center items-center py-[10px] px-[21px] rounded-[12px] bg-[#dfdfdf] text-[16px] w-[30%] gap-[15px] whitespace-nowrap'
-                // onClick={() =>
-                //   router.push(
-                //     `/dashboard/product/${productId}/addComparison?category_level_1_product_id=${payload.category_level_1_id}`
-                //   )
-                // }
+                onClick={() => router.push(`/dashboard/variant/${id}/create`)}
               >
                 <Image
                   src={CirclePlus}
@@ -271,8 +347,21 @@ export default function ProductForm(
                   'Status',
                   'Date created',
                   'Image',
-                  'Detail',
-                  'Option',
+                  <div className='flex justify-center'>
+                    <Image
+                      src={ReturnIcon}
+                      className='w-[20px] h-[15px]'
+                      alt='return_icon'
+                    />
+                  </div>,
+                  <div className='flex justify-center'>
+                    <Image
+                      src={SliderIcon}
+                      alt='slider_icon'
+                      className='w-[20px] h-[20px] cursor-pointer'
+                      onClick={() => setFilterModal((prev) => !prev)}
+                    />
+                  </div>,
                 ]}
                 listKey={[
                   'name',
@@ -373,13 +462,9 @@ export default function ProductForm(
               </div>
             </div>
             <div className='col-span-3 flex gap-[20px] '>
-              <button
+              <Link
                 className='flex justify-center items-center py-[10px] px-[21px] rounded-[12px] bg-[#dfdfdf] text-[16px] w-[30%] gap-[15px] whitespace-nowrap'
-                // onClick={() =>
-                //   router.push(
-                //     `/dashboard/product/${productId}/addComparison?category_level_1_product_id=${payload.category_level_1_id}`
-                //   )
-                // }
+                href={`/dashboard/comparison/${id}/create`}
               >
                 <Image
                   src={CirclePlus}
@@ -387,7 +472,7 @@ export default function ProductForm(
                   className='w-[15px] h-[15px]'
                 />{' '}
                 Create New
-              </button>
+              </Link>
               <div className='relative w-[80%]'>
                 <input
                   name='brand'
@@ -410,8 +495,21 @@ export default function ProductForm(
                   'Status',
                   'Date created',
                   'Image',
-                  'Detail',
-                  'Option',
+                  <div className='flex justify-center'>
+                    <Image
+                      src={ReturnIcon}
+                      className='w-[20px] h-[15px]'
+                      alt='return_icon'
+                    />
+                  </div>,
+                  <div className='flex justify-center'>
+                    <Image
+                      src={SliderIcon}
+                      alt='trash_icon'
+                      className='w-[20px] h-[20px]'
+                      onClick={() => setFilterModal((prev) => !prev)}
+                    />
+                  </div>,
                 ]}
                 listKey={[
                   'is_primary',
@@ -421,9 +519,81 @@ export default function ProductForm(
                   'created_at',
                   'image',
                   'detail',
-                  'option',
+                  'delete',
                 ]}
-                data={comparison}
+                data={comparison.map((item) => ({
+                  ...item,
+                  image: (
+                    <Image
+                      src={item.image || NoImage}
+                      width={150}
+                      height={100}
+                      alt={`gambar`}
+                      className='rounded-md'
+                    />
+                  ),
+                  is_active: (
+                    <span
+                      className={`table_status ${
+                        item.is_active === 1 ? 'publish' : 'draft'
+                      }`}
+                    >
+                      {item.is_active ? 'Publish' : 'Draft'}
+                    </span>
+                  ),
+                  created_at: formatDate(item.created_at),
+                  action: (
+                    <div className='flex flex-col gap-[10px]'>
+                      <Link
+                        href={`/dashboard/variant/${item.object_id}`}
+                        className='w-full'
+                      >
+                        <Button
+                          borderRadius='5px'
+                          bgColor='rgba(101, 57, 228, 0.58)'
+                          color='#fff'
+                          text='Variant List'
+                          width='100%'
+                          padding='3.5px'
+                        />
+                      </Link>
+                      <Link href={`/dashboard/comparison/${item.object_id}`}>
+                        <Button
+                          borderRadius='5px'
+                          bgColor='rgba(228, 57, 57, 0.58)'
+                          color='#fff'
+                          text='Comparison List'
+                          width='100%'
+                          padding='3.5px'
+                        />
+                      </Link>
+                    </div>
+                  ),
+                  detail: (
+                    <div className='flex justify-center'>
+                      <Image
+                        src={PencilIcon}
+                        className='w-[20px] h-[20px]'
+                        alt='return_icon'
+                        onClick={() =>
+                          router.push(
+                            `/dashboard/variant/${id}/${item.object_id}`
+                          )
+                        }
+                      />
+                    </div>
+                  ),
+                  delete: (
+                    <div className='flex justify-center'>
+                      <Image
+                        src={TrashIcon}
+                        className='w-[20px] h-[20px]'
+                        alt='trash_icon'
+                        // onClick={() => callDeleteVariant(id, item.object_id)}
+                      />
+                    </div>
+                  ),
+                }))}
                 type={'product'}
                 id={''}
               />
